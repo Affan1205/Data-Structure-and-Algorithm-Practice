a68b43b8c0a5ccd54028333005c2a369e66d94d0
Shaik Affan Ahemad <shaikaffanahemad@gmail.com>
2025-09-19 21:44:24 +0530

    Day 2

---
+++ 04.Function(Methods)/PracticeQuesFunction07.java
@@ -0,0 +1,7 @@
+//Define a method to find out if a number is prime or not.
+public class PracticeQuesFunction07 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+    
+}
